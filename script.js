fe2de667587f580eceb5edd9415cf8b58aae510d
hypnotixx0 <133892941+hypnotixx0@users.noreply.github.com>
2025-09-29 15:52:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-import { useRouter } from "next/router";
+// If the page has no query string, add one automatically
+if (!window.location.search) {
+  // Generate a random "state" param to make it look unique each time
+  const randomState = Math.random().toString(36).substring(2, 18);
 
-export default function DistrictPicker() {
-  const router = useRouter();
-  const { channel, client_id, district_id } = router.query;
+  const query = `/oauth/district-picker?channel=clever&client_id=4c63c1cf623dce82caac&confirmed=true&district_id=5931b9f7abace5000122d686&redirect_uri=https%3A%2F%2Fclever.com%2Fin%2Fauth_callback&response_type=code&state=${randomState}`;
 
-  return (
-    <div>
-      <h1>OAuth District Picker</h1>
-      <p>Channel: {channel}</p>
-      <p>Client ID: {client_id}</p>
-      <p>District ID: {district_id}</p>
-    </div>
-  );
+  // Redirect with the fake query string
+  window.location.href = window.location.origin + window.location.pathname + query;
 }
