e5842da71adfa5627d163394b437cd0cfb0298df
hypnotixx0 <133892941+hypnotixx0@users.noreply.github.com>
2025-09-29 15:54:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// If the page has no query string, add one automatically
 if (!window.location.search) {
-  // Generate a random "state" param to make it look unique each time
+  // Random "state" parameter
   const randomState = Math.random().toString(36).substring(2, 18);
 
-  const query = `/oauth/district-picker?channel=clever&client_id=4c63c1cf623dce82caac&confirmed=true&district_id=5931b9f7abace5000122d686&redirect_uri=https%3A%2F%2Fclever.com%2Fin%2Fauth_callback&response_type=code&state=${randomState}`;
+  // Just the query string (no extra path!)
+  const query = "?channel=clever&client_id=4c63c1cf623dce82caac&confirmed=true&district_id=5931b9f7abace5000122d686&redirect_uri=https%3A%2F%2Fclever.com%2Fin%2Fauth_callback&response_type=code&state=" + randomState;
 
-  // Redirect with the fake query string
+  // Redirect with the query
   window.location.href = window.location.origin + window.location.pathname + query;
 }
